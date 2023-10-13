185c47b8b5ad9f47c1a6d796835043262efcc735
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-13 14:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  if (num === 0) {
+    return '0';
+  }
+
+  let binary = '';
+
+  while (num > 0) {
+    binary = (num % 2) + binary;
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+// Example usage:
+console.log(decimalToBinary(7)); // Output: "111"
+console.log(decimalToBinary(10)); // Output: "1010"
+console.log(decimalToBinary(33)); // Output: "100001"
